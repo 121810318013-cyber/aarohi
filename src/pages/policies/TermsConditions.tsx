1c4b3b2d6f11e42245e75f61ed663bcd384035e6
pswaroop <sswaroop46@gmail.com>
2025-12-22 17:03:35 +0530

    Updated

--- src/pages/policies/TermsConditions.tsx
+++ src/pages/policies/TermsConditions.tsx
@@ -3,13 +3,15 @@ import React from "react";
 const TermsConditions: React.FC = () => {
   return (
     <div id="terms" className="policy-section">
-      <h2>Terms & Conditions</h2>
+      <h1>Terms & Conditions</h1>
+      <br />
       <p className="intro-text">
         This document is an electronic record under the Information Technology
         Act, 2000. By using <strong>aarohiselections.com</strong> Platform, you
         agree to these binding terms.
       </p>
-
+      <br />
+      <br />
       <div className="policy-grid">
         <section className="policy-card">
           <h3>1. Platform Usage</h3>
@@ -19,7 +21,7 @@ const TermsConditions: React.FC = () => {
             <li>Comply with all applicable Indian laws</li>
           </ul>
         </section>
-
+        <br />
         <section className="policy-card">
           <h3>2. Intellectual Property</h3>
           <p>
@@ -27,7 +29,7 @@ const TermsConditions: React.FC = () => {
             Unauthorized use may lead to legal action.
           </p>
         </section>
-
+        <br />
         <section className="policy-card">
           <h3>3. Payments & Transactions</h3>
           <p>
@@ -35,7 +37,7 @@ const TermsConditions: React.FC = () => {
             create legally binding contracts.
           </p>
         </section>
-
+        <br />
         <section className="policy-card">
           <h3>4. Indemnity</h3>
           <p>
@@ -43,7 +45,7 @@ const TermsConditions: React.FC = () => {
             breach of these terms.
           </p>
         </section>
-
+        <br />
         <section className="policy-card">
           <h3>5. Governing Law</h3>
           <p>
